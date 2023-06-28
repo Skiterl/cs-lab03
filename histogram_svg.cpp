8fdef902e228b272560e6530c657b3a6425432d2
Alex Zharkov <alexzhar03@gmail.com>
2023-06-28 15:12:16 +0300

    code: выделение в модуль функций для работы с SVG

---
+++ histogram_svg.cpp
@@ -0,0 +1,51 @@
+#include "histogram_svg.h"
+
+void svg_begin(double width, double height) {
+    std::cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    std::cout << "<svg ";
+    std::cout << "width='" << width << "' ";
+    std::cout << "height='" << height << "' ";
+    std::cout << "viewBox='0 0 " << width << " " << height << "' ";
+    std::cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void svg_text(double left, double baseline, std::string text) {
+    std::cout << "<text x='" << left << "' y='" << baseline << "'>" + text + "</text>";
+}
+void svg_end() {
+    std::cout << "</svg>\n";
+}
+
+void svg_rect(double x, double y, double width, double height,
+    std::string stroke = "black", std::string fill = "black") {
+    std::cout << "<rect x = '" << x << "' y = '" << y << "' width = '" << width << "' height = '" << height << "' stroke = '"
+        << stroke << "' fill = '" << fill << "'/>";
+}
+
+void show_histogram_svg(const std::vector<size_t>& bins, std::vector<std::string> colors) {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    auto max = *std::ranges::max_element(bins);
+
+    double top = 0;
+    size_t color_index = 0;
+    for (size_t bin : bins) {
+        double bin_width = bin;
+        if (max > 40) {
+            bin_width = BLOCK_WIDTH * 40 * bin_width / max;
+        }
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, std::to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, colors[color_index], colors[color_index]);
+        top += BIN_HEIGHT;
+        color_index++;
+    }
+    svg_end();
+}
---
+++ histogram_svg.h
@@ -0,0 +1,16 @@
+#ifndef HISTOGRAM_SVG_H_INCLUDED
+#define HISTOGRAM_SVG_H_INCLUDED
+
+#include <vector>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void svg_begin(double width, double height);
+void svg_text(double left, double baseline, std::string text);
+void svg_end();
+void svg_rect(double x, double y, double width, double height,
+    std::string stroke, std::string fill);
+void show_histogram_svg(const std::vector<size_t>& bins, std::vector<std::string> colors);
+#endif
