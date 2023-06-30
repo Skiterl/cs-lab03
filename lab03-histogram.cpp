d55df9242d23cbc7d66cc2ad7caaa0340bffd8e2
Alex Zharkov <alexzhar03@gmail.com>
2023-06-30 17:35:44 +0300

    code: печать содержимого файла из сети на стандартный вывод

--- lab03-histogram.cpp
+++ lab03-histogram.cpp
@@ -102,7 +102,12 @@ int main(int argc, char* argv[])
 {
     if (argc > 1) {
         CURL* curl = curl_easy_init();
-        
+        if (curl) {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+        }
         return 0;
     }
     const auto input = read_input(std::cin, false);
